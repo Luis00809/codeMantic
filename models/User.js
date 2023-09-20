37ff2ab23f61c2de3305c6f87dfcce032bef22d1
Luis00809 <davidcarvajal008@gmail.com>
2023-09-19 22:03:35 -0700

    creates user and form models

--- models/User.js
+++ models/User.js
@@ -1,1 +1,61 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {};
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    }, 
+    display_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        
+        validate: {
+            len: [5],
+        },
+    }, 
+    pronouns: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+
+        validate: {
+            isEmail: true,
+        },
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [8],
+        },
+    },
+},
+{
+    hooks: {
+        beforeCreate: async (newData) => {
+            newData.password = await bcrypt.hash(newData.password, 10);
+            return newData;
+        },
+        beforeUpdate: async (updatedData) => {
+            if(updatedData.password) {
+                updatedData.password = await bcrypt.hash(updatedData.password, 10);
+            }
+            return updatedData;
+        },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+}
+);
