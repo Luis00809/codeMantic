8a043f4cc75a3631e595fa94eae235e4818eff4e
Donnie Rawlings <tacobellboi@yahoo.com>
2023-09-20 22:06:58 -0500

    seed.js initialize

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,4 +1,28 @@
+const sequelize = require('../config/connection');
+const { User, Form } = require('../models');
 
+const userData = require('./userData.json');
+const formData = require('./projectData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const form of formData) {
+    await Form.create({
+      ...form,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
 
 
 
