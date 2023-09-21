37306c5534db0b065a5e59e7f42a8b2440f215fd
Luis00809 <davidcarvajal008@gmail.com>
2023-09-20 23:33:32 -0700

    creates api form routes

--- controllers/api/formRoutes.js
+++ controllers/api/formRoutes.js
@@ -1,1 +1,33 @@
-const 
+const router = require('express').Router();
+const Form = require('../../models/Form');
+
+// create a form 
+router.post('/', async (req, res) => {
+    try{
+        const formData = await Form.create(req.body);
+        res.status(200).json(formData);
+
+    } catch (err) {
+        res.status(400).json(err)
+    }
+});
+
+// update a form
+router.put('/update/:id', async (req, res) => {
+    try {
+
+        const formData = await Form.update(req.body, 
+            {
+                where: {
+                    id: req.body.id,
+                },
+            });
+
+        res.status(200).json(formData);
+
+    } catch (err) {
+        res.status(400).json(err)
+    }
+});
+
+module.exports = router;
---
+++ controllers/form-routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const Form = require('../models/Form');
+
+// gets 
+
+module.exports = router;
