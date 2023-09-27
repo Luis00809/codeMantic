29e5c6340e79f583ebd8a4af41e56ab723f4b338
Luis00809 <davidcarvajal008@gmail.com>
2023-09-27 13:09:25 -0700

    filters out users

--- controllers/api/formRoutes.js
+++ controllers/api/formRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const Form = require('../../models/Form');
 const User = require('../../models/User');
-
+const { Op } = require('sequelize');
 router.get('/forms', async (req, res) => {
     try {
 
@@ -46,35 +46,58 @@ router.put('/update/:id', async (req, res) => {
 // grab filtered users: 
 // Our client side fetch would look something like this: 
 // GET /filteredList/?languages=JavaScript&ageRange=18-25
+// for languages the query would look like: languages=Python,Javascript for multiple
 
-router.get('/filteredList/', async (req, res) => {
-    try{
-        // later switch pronounse into form model or find a way to also grab bio from User model
-        const { languages, bio, personality_type, ageRange, operating_system, hobbies  } = req.query;
-
-        const filter = {
-            languages,
-            bio,
-            personality_type,
-            ageRange,
-            operating_system,
-            hobbies
+router.get('/filteredList', async (req, res) => {
+    try {
+        // Extract filter parameters from the query string
+        const { operating_system, languages, partner_pronouns, personality_type } = req.query;
+    
+        // Create an empty object to store the conditions for filtering
+        const whereConditions = {};
+    
+        // Add conditions for each filter parameter if they are provided
+        if (operating_system) {
+          whereConditions.operating_system = operating_system;
+        }
+        if (languages) {
+          // Split the comma-separated values into an array
+          const languageList = languages.split(',');
+          
+          // Create an array of conditions for each language
+          const languageConditions = languageList.map(language => ({
+            languages: {
+              [Op.like]: `%${language.trim()}%`, // Partial match for each language
+            },
+          }));
+          
+          // Combine multiple conditions with OR operator
+          whereConditions[Op.or] = languageConditions;
+        }
+        if (partner_pronouns) {
+          whereConditions.partner_pronouns = partner_pronouns;
+        }
+        if (personality_type) {
+          whereConditions.personality_type = personality_type;
         }
 
-        const filteredUsers = await User.findAll({
-            include: {
-                model: Form,
-                where: filter
-            }
-        });
+    // Use Sequelize's findAll method to filter users
+    const filteredUsers = await User.findAll({
+      include: [
+        {
+          model: Form,
+          where: whereConditions, // Apply the conditions here
+        },
+      ],
+    });
 
-        res.status(200).json(filteredUsers);
-
-    } catch (err) {{
-        res.status(400).json(err)
-        console.log(err);
-    }}
+    res.json(filteredUsers);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
+
 
 
 
